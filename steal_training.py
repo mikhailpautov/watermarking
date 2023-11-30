bda84ba11a9846d916fbfb26254c4ab3a8ce3906
BGDNick <49807419+BGDNick@users.noreply.github.com>
2023-11-30 14:46:31 +0300

    Update steal_training.py

--- steal_training.py
+++ steal_training.py
@@ -6,10 +6,11 @@ os.environ["CUDA_VISIBLE_DEVICES"]="4"
 
 import torch
 import torchvision.datasets as datasets
+from torch.utils.data import DataLoader, Subset
 
 from models import resnet34
 
-from utils import steal_train, models_mse, get_dataset
+from utils import steal_train, models_mse, get_dataset, evaluate_model, flip_labels
 
 from absl import app, flags
 
@@ -39,12 +40,27 @@ def main(argv):
 
     flip_indexes = data['in_data']
 
+    trainset_cifar10, testset_cifar10 = get_dataset('cifar10')
+    flipset_cifar10, _ = get_dataset('cifar10')
+
+    flip_labels(flipset_cifar10, indices=flip_indexes)
+    trainset_cifar10 = Subset(trainset_cifar10, list(set(range(50000)) - set(flip_indexes)))
+    flipset_cifar10 = Subset(flipset_cifar10, flip_indexes)
+
+    trainloader_cifar10 = DataLoader(trainset_cifar10, batch_size=512, shuffle=True)
+    fliploader_cifar10 = DataLoader(flipset_cifar10, batch_size=512, shuffle=True)
+    testloader_cifar10 = DataLoader(testset_cifar10, batch_size=512, shuffle=True)
+
     for i in range(num_models):
         temp_model = resnet34(pretrained=False)
         steal_train(model, temp_model, trainset, testset, num_epochs, device, 
                     do_eval=True, epoch_eval=5, batch_size=batch_size)
         mse_value = models_mse(temp_model, model)
         torch.save(temp_model.state_dict(), save_path + 'model_mse_' + str(round(mse_value, 3)))
+        train_acc_cifar10 = evaluate_model(temp_model, trainloader_cifar10, device)
+        flip_acc_cifar10 = evaluate_model(temp_model, fliploader_cifar10, device)
+        test_acc_cifar10 = evaluate_model(temp_model, testloader_cifar10, device)
+        print(f'Model {i+1} train. Train acc {train_acc_cifar10:.3f} Flip acc {flip_acc_cifar10:.3f} Test acc {test_acc_cifar10:.3f}')
 
 if __name__ == '__main__':
     flags.DEFINE_integer('batch_size', 512, 'Batch size')
