a709751b097cc70d97044823aa49a8f590ecb0f0
Oleg <25301004+olegrgv@users.noreply.github.com>
2024-01-24 23:27:52 +0300

    Update steal_training.py

--- steal_training.py
+++ steal_training.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 import torch
